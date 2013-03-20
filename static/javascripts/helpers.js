92513a6eacffc686b395195e3c6fbade5f72cf6a
Thomas Boyt <me@thomasboyt.com>
2013-03-20 16:05:20 -0400

    Better performance, hopefully

--- static/javascripts/helpers.js
+++ static/javascripts/helpers.js
@@ -4,17 +4,17 @@ define([], {
   chatOpen: false,
   showChatEntry: function(e) {
     console.log("Showing chat entry");
+    this.chatOpen = true;
     $("#bottom-bar").addClass("hidden");
     $("#chat-entry").addClass("visible");
     $("#chat-container").addClass("withChatEntry");
-    this.chatOpen = true;
   },
   hideChatEntry: function(e) {
     console.log("Hiding chat entry");
+    this.chatOpen = false;
     $("#chat-entry").removeClass("visible");
     $("#bottom-bar").removeClass("hidden");
     $("#chat-container").removeClass("withChatEntry");
-    this.chatOpen = false;
   }
 });
 
